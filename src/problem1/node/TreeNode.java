dd5b1e5655b534ca8e908d3b6ac921523971548c
Isha Gupta <isha.gupta_cs18@gla.ac.in>
2020-03-28 18:48:30 +0530

    getter setter of data, left and right for the TreeNode

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -27,4 +27,29 @@ public class TreeNode {
         left = right = null;
 
     }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }
+
