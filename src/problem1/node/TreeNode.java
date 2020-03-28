8611cdbf365371af2f9e5a0731427bc81d0218a8
Isha Gupta <isha.gupta_cs18@gla.ac.in>
2020-03-28 18:47:02 +0530

    creation of default constructor

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,9 +6,25 @@
  */
 package problem1.node;
 
+import java.util.Scanner;
+
 public class TreeNode {
     private TreeNode left;
     private TreeNode right;
     private int data;
 
+    public TreeNode(TreeNode left, TreeNode right, int data) {
+        this.left = left;
+        this.right = right;
+        this.data = data;
+
+    }
+
+    //default constructor
+    public TreeNode() {
+        System.out.print("Enter integer value : ");
+        data = new Scanner(System.in).nextInt();
+        left = right = null;
+
+    }
 }
