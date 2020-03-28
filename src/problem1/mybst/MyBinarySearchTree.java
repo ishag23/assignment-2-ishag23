1e24290992ebd63d9fd0edc00d2a688520d76b41
Isha Gupta <isha.gupta_cs18@gla.ac.in>
2020-03-28 18:55:47 +0530

    creation of root,tmp,pre and post

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,25 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    private static int count;
+    private TreeNode newnode, root, tmp;
+    private MyQueue pre;
+    private MyQueue post;
+
+    public MyBinarySearchTree(TreeNode newnode) {
+        this.newnode = newnode;
+        tmp = null;
+        root = null;
+        MyBinarySearchTree.count = 0;
+        post = new MyQueue();
+        pre = new MyQueue();
+    }
+
 }
