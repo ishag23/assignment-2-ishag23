398ac7412aaf4ec14cb5f210997090f39dc89ab8
Isha Gupta <isha.gupta_cs18@gla.ac.in>
2020-03-30 23:40:42 +0530

    method for add

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,59 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private Node front, tmp, end;
+
+    public MyCircularQueue() {
+        front = null;
+        tmp = null;
+        end = null;
+    }
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public Node getTmp() {
+        return tmp;
+    }
+
+    public void setTmp(Node tmp) {
+        this.tmp = tmp;
+    }
+
+    public Node getEnd() {
+        return end;
+    }
+
+    public void setEnd(Node end) {
+        this.end = end;
+    }
+
+    public void add(Node newNode) {
+        if (front == null) {
+            tmp = front = newNode;
+            return;
+        }
+        if (tmp.getNext() == null) {
+            tmp.setNext(newNode);
+            newNode.setNext(tmp);
+            end = newNode;
+            return;
+
+        }
+        newNode.setNext(tmp.getNext());
+        tmp.setNext(newNode);
+    }
+
+
+
 }
